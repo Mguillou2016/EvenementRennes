e5e7ed8ee864e99e5f574e9e49292b00e5be949f
Mguillou2016 <mael.guillou2016@campus-eni.fr>
2018-05-30 11:21:34 +0200

    Création des models

---
+++ EvenementRennes/Models/Evenement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EvenementRennes.Models
+{
+    public class Evenement
+    {
+        public string lieu { get; set; }
+        public DateTime date { get; set; }
+        public TimeSpan duree { get; set; }
+        public string theme { get; set; }
+        public string descriptif { get; set; }
+        public List<string> images { get; set; }
+    }
+}
---
+++ EvenementRennes/Models/Parking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EvenementRennes.Models
+{
+    public class Parking
+    {
+        public Dictionary<string, string> etats { get; set; }
+        public string geolocalisation { get; set; }
+        
+    }
+}
