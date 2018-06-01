dbebaf363b5ca563d83a6fe4a04fd7434f52761b
Mguillou2016 <mael.guillou2016@campus-eni.fr>
2018-06-01 14:54:59 +0200

    Création d'évenements de tests, mise a jour de l'index, ajout d'une action liée aux trajets

--- EvenementRennes/Controllers/ConviveController.cs
+++ EvenementRennes/Controllers/ConviveController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using EvenementRennes.Models;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +10,60 @@ namespace EvenementRennes.Controllers
 {
     public class ConviveController : Controller
     {
-        // GET: Convive
-        public ActionResult Index()
+        public string adresse = "test";
+        List<Evenement> listE = new List<Evenement>();
+        List<string> listeI = new List<string>();
+
+        public ConviveController()
         {
-            return View();
+            listeI.Add("test");
+            listE.Add(new Evenement(Guid.NewGuid(), "rennes", new DateTime(), new TimeSpan(), "a", "Mox dicta finierat, multitudo omnis ad, quae imperator voluit, promptior laudato consilio consensit in pacem ea ratione maxime percita, quod norat expeditionibus crebris fortunam eius in malis tantum civilibus vigilasse, cum autem bella moverentur externa, accidisse plerumque luctuosa, icto post haec foedere gentium ritu perfectaque sollemnitate imperator Mediolanum ad hiberna discessit.", listeI));
+            listE.Add(new Evenement(Guid.NewGuid(), "rennes", new DateTime(), new TimeSpan(), "a", "b", listeI));
+            listE.Add(new Evenement(Guid.NewGuid(), "rennes", new DateTime(), new TimeSpan(), "a", "b", listeI));
+            listE.Add(new Evenement(Guid.NewGuid(), "rennes", new DateTime(), new TimeSpan(), "a", "b", listeI));
+
+            listE.Add(new Evenement(Guid.NewGuid(), "rennes", new DateTime(), new TimeSpan(), "a", "b", listeI));
+
+            listE.Add(new Evenement(Guid.NewGuid(), "rennes", new DateTime(), new TimeSpan(), "a", "b", listeI));
+
+            listE.Add(new Evenement(Guid.NewGuid(), "rennes", new DateTime(), new TimeSpan(), "a", "b", listeI));
+
+            listE.Add(new Evenement(Guid.NewGuid(), "rennes", new DateTime(), new TimeSpan(), "a", "b", listeI));
+
+
+        }
+        // GET: Convive
+        public ActionResult IndexConvive()
+        {
+            ViewBag.Adresse = adresse;
+            return View(listE);
+        }
+        // POST: Convive
+        [HttpPost]
+        public ActionResult IndexConvive(FormCollection collection)
+        {
+            ViewBag.adresse = collection.Get("Adresse");
+            //string adresse = collection.Get("Adresse");
+            //Debug.WriteLine("adresse", adresse);
+            return View(listE);
+        }
+        [HttpPost]
+        public ActionResult TrajetConvive(FormCollection collection)
+        {
+            Debug.WriteLine("inside");
+            Debug.WriteLine("adresse", collection.Get("adresse"));
+            ViewData["adresse"] =  collection.Get("adresse");
+
+            /*foreach(string element in collection)
+            {
+                Debug.WriteLine("test", element);
+            }*/
+            return View("trajetConvive");
+        }
+        [Route("convive/someAction")]
+        public ActionResult SomeAction()
+        {
+            return Json(new { SomeValue = "foo bar" }, JsonRequestBehavior.AllowGet);
         }
     }
 }
