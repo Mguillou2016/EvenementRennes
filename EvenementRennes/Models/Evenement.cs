54a583141b7a503c0a136c62b5e4f516f7edd174
Mguillou2016 <mael.guillou2016@campus-eni.fr>
2018-06-01 14:55:35 +0200

    ajout d'un constructeur par défaut

--- EvenementRennes/Models/Evenement.cs
+++ EvenementRennes/Models/Evenement.cs
@@ -7,6 +7,17 @@ namespace EvenementRennes.Models
 {
     public class Evenement
     {
+        public Evenement(Guid iD, string lieu, DateTime date, TimeSpan duree, string theme, string descriptif, List<string> images)
+        {
+            ID = iD;
+            this.lieu = lieu;
+            this.date = date;
+            this.duree = duree;
+            this.theme = theme;
+            this.descriptif = descriptif;
+            this.images = images;
+        }
+
         public Guid ID { get; set; }
         public string lieu { get; set; }
         public DateTime date { get; set; }
@@ -14,5 +25,7 @@ namespace EvenementRennes.Models
         public string theme { get; set; }
         public string descriptif { get; set; }
         public List<string> images { get; set; }
+
+       
     }
 }
