044f6c340ba192e568dd3258fa52ee7fb4da89fd
Mguillou2016 <mael.guillou2016@campus-eni.fr>
2018-06-07 16:21:13 +0200

    ajout de la durée pour le calcul des tarifs

--- EvenementRennes/Controllers/ConviveController.cs
+++ EvenementRennes/Controllers/ConviveController.cs
@@ -2,9 +2,15 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.VisualBasic.FileIO;
+using WebGrease.Css.Ast.Selectors;
+using WebGrease.Css.Extensions;
+using FieldType = Microsoft.Ajax.Utilities.FieldType;
 
 namespace EvenementRennes.Controllers
 {
@@ -50,12 +56,78 @@ namespace EvenementRennes.Controllers
         [HttpPost]
         public ActionResult TrajetConvive(FormCollection collection)
         {
-            Debug.WriteLine("inside");
-            Debug.WriteLine("adresse", collection.Get("adresse"));
-            Debug.WriteLine("lieu", collection.Get("lieu"));
+            Console.WriteLine("inside");
+            Console.WriteLine("adresse: "+ collection.Get("adresse"));
+            Console.WriteLine("lieu: "+ collection.Get("lieu"));
+            Console.WriteLine("duree: "+ collection.Get("duree"));
             ViewData["adresse"] =  collection.Get("adresse");
             ViewData["lieu"] = collection.Get("lieu");
+            ViewData["duree"] =  collection.Get("duree");
+
             
+            /*WebRequest request = WebRequest.Create ("http://data.citedia.com/r1/parks/timetable-and-prices");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse ();
+            Stream dataStream = response.GetResponseStream ();
+            StreamReader reader = new StreamReader (dataStream);
+            string responseFromServer = reader.ReadToEnd ();
+            
+            Console.WriteLine (responseFromServer);
+            reader.Close ();
+            dataStream.Close ();
+            response.Close ();*/
+
+
+//            Console.WriteLine(" test: "+System.IO.File.ReadAllLines(responseFromServer).Skip(1));
+              /*  select new
+                {
+                    
+
+                    Plant = columns[0],
+                    Material = int.Parse(columns[1]),
+                    Density = float.Parse(columns[2]),
+                    StorageLocation = int.Parse(columns[3])
+
+                }*/
+            
+            /*var allTextLines = responseFromServer.Split('\n');
+            allTextLines.ForEach((lines) =>
+            {
+                Console.WriteLine("lines: "+lines);
+                var separatesDatas = lines.Split(';');
+                separatesDatas.ForEach((data) =>
+                {
+                    Console.WriteLine("separatesDatas: " + data);
+                });
+            });*/
+             
+            
+            /*TextFieldParser parser = new TextFieldParser(responseFromServer);
+            parser.TextFieldType = FieldType.Delimited;
+            parser.SetDelimiters(",");
+            while (!parser.EndOfData) 
+            {
+                //Processing row
+                string[] fields = parser.ReadFields();
+                foreach (string field in fields) 
+                {
+                    //TODO: Process field
+                }
+            }*/
+            
+            /*using (TextFieldParser parser = new TextFieldParser(@"c:\temp\test.csv"))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                while (!parser.EndOfData) 
+                {
+                    //Processing row
+                    string[] fields = parser.ReadFields();
+                    foreach (string field in fields) 
+                    {
+                        //TODO: Process field
+                    }
+                }
+            }            */
 
             /*foreach(string element in collection)
             {
